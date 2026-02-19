ee2ce17f08585c5b3cf1dadf2da00a8834406632
Shrey-Arc <shreykumarsks@gmail.com>
2026-02-19 10:11:38 +0530

    Welcome Message:

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,18 +1,7 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        // 1. Program starts & Hardcoded string is defined
-        String input = "radar";
-
-        // 2. Hardcoded string is checked
-        String cleanInput = input.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
-        String reversed = new StringBuilder(cleanInput).reverse().toString();
-
-        boolean isPalindrome = cleanInput.equals(reversed);
-
-        // 3. Result is printed
-        System.out.println("String: " + input);
-        System.out.println("Is Palindrome? " + isPalindrome);
-
-        // 4. Program exits
+        System.out.println("Welcome to Palindrome Checker Management System\n" +
+                "Version 1.0\n" +
+                "System Initialised Successfully.");
     }
 }
